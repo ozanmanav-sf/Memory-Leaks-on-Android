8b42f41e9b263c57399476b8d6bfedfcd73841cb
hasretsariyer <hasretsariyer@gmail.com>
2020-02-20 12:34:30 +0300

    Comment out LeakCanary.install

--- app/src/main/java/com/codingwithmitch/memoryleaks/MyApplication.java
+++ app/src/main/java/com/codingwithmitch/memoryleaks/MyApplication.java
@@ -9,13 +9,13 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        // Normal app init code...
+//         if (LeakCanary.isInAnalyzerProcess(this)) {
+//             // This process is dedicated to LeakCanary for heap analysis.
+//             // You should not init your app in this process.
+//             return;
+//         }
+//         LeakCanary.install(this);
+//         // Normal app init code...
     }
 }
 
